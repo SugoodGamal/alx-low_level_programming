0cc28d73ee15383fd7f9d6e23846fcf61ffd2870
SugoodGamal <sujood74g@gmail.com>
2023-09-09 20:37:47 -0700

    kkk

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int gigit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',')
+			putchar(' ')
+		}
+		diit++;
+	}
+	puchar('\n');
+	return (0);
+}
