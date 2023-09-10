d4fa109ebdf600e99303fe160369e3e22c847322
SugoodGamal <sujood74g@gmail.com>
2023-09-10 14:20:02 -0700

    ddd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,36 +10,36 @@
 */
 int main(void)
 {
-	int digit1 = 0;
-	int digit2, digit3;
+		int digit1 = 0;
+		int digit2, digit3;
 
-	while (digit1 <= 9)
-	{
-		digit2 = 0;
-		while (digit2 <= 9)
+		while (digit1 <= 9)
 		{
-			if (digit1 != digit2 &&
-				digit1 < digit2 &&
-				digit2 != digit3 &&
-				digit2 < digit3)
+			digit2 = 0;
+			while (digit2 <= 9)
 			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
-				putchar(digit3 + 48);
-
-				if (digit1 + digit2 + digit3 != 24)
+				if (digit1 != digit2 &&
+					digit1 < digit2 &&
+					digit2 != digit3 &&
+					digit2 < digit3)
 				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			digit3++;
-		}
-		digit2++;
-	}
-	digit1++;
-}
-putchar('\n');
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
 
-return (0);
+					if (digit1 + digit2 + digit3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				digit3++;
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+
+	return (0);
 }
