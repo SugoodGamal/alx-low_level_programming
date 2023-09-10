829289ff69d0c9211a73c0e6ebacceaaf6e5d45b
SugoodGamal <sujood74g@gmail.com>
2023-09-09 19:41:01 -0700

    kkk

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("/n");
+
+	return (0);
+}
