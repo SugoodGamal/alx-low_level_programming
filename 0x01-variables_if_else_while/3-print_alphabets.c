a5b7923904b0646ccc3c46e9ef8443d73b280cb9
SugoodGamal <sujood74g@gmail.com>
2023-09-09 19:30:23 -0700

    kkk

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,19 +8,17 @@
  * Return: Always 0 (Success)
 */
 int main(void)
-
 {
 	char ch = 'a';
-	char ch = 'A';
+	char CH = 'A';
 	/*prints a - z*/
-	while (ch <= 'z');
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-        
 	/*prints A - Z*/
-	while (ch <= 'Z');
+	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
