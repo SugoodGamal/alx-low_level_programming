63e72cf6177afe6258c68433dbd04465a73bf5ea
SugoodGamal <sujood74g@gmail.com>
2023-09-19 05:33:08 -0700

    kkk

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,15 +4,15 @@
  * @s: string
  * return: 0
 */
-int  print_rev(char *s)
+void  print_rev(char *s)
 {
 	int longi = 0;
 	int o;
 
 	while (*s != '\0')
 	{
-	longi++;
-	s++;
+		longi++;
+		s++;
 	}
 	s--;
 	for (o = longi; o > 0; o--)
