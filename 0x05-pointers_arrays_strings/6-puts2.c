968d08d9750db8d2b41300de0242c08ac5328781
SugoodGamal <sujood74g@gmail.com>
2023-09-19 11:04:13 -0700

    ggg

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include <main.h>
 /**
- * puts - function should print only one character out of
+ * puts - function should print only one character out
  * starting with the first one
  * @str: input
  * Return: print
