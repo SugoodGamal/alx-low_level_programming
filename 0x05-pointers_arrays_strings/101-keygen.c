2948b4d9ad2d0ae103f4e6bf0fd52051b788fff4
SugoodGamal <sujood74g@gmail.com>
2023-09-19 17:14:18 -0700

    lll

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,9 +14,9 @@ int main(void)
 	srand(time(NULL));
 	while (sum <= 2645)
 	{
-			c = rand() % 128;
-			sum += c;
-			putchar(c);
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
 	putchar(2772 - sum);
 	return (0);
