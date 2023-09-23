4ce492705b42fc0257198bf30aa9a678e01e871c
SugoodGamal <sujood74g@gmail.com>
2023-09-22 23:45:03 -0700

    lll

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,10 +12,9 @@ char *rot13(char *s)
 	int i;
 	int j;
 	char data1[] =
-"ABCDEFGIHJKLMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz";
+"ABCDEFGHIJKLMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] =
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
