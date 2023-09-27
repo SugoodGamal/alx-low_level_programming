3f85724eb2c8e76dc38754efc7db183bf0e11388
SugoodGamal <sujood74g@gmail.com>
2023-09-26 18:30:27 -0700

    kkk

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,6 +4,7 @@
 /**
  * rot13 - encoder rot13
  * @s: pointer to string params
+ *
  * Return: *s
 */
 
@@ -12,14 +13,14 @@ char *rot13(char *s)
 	int i;
 	int j;
 	char data1[] =
-"ABCDEFGHIJKLMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz";
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] =
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == data1[i])
+			if (s[i] == data1[j])
 			{
 				s[i] = datarot[j];
 				break;
